9a70a77b68fc77a2ea7604660e0d8cbe4aaf82e3
christophecatherine <73233923+christophecatherine@users.noreply.github.com>
2023-01-18 10:12:54 +0100

    Updates

---
+++ precache-manifest.fed11fe289efcc079484bb48dc7575e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6b4ae34182fec9dd50f",
+    "url": "/projet4/static/css/main.33605159.chunk.css"
+  },
+  {
+    "revision": "d6b4ae34182fec9dd50f",
+    "url": "/projet4/static/js/main.d6b4ae34.chunk.js"
+  },
+  {
+    "revision": "76a0401290bd0fa73966",
+    "url": "/projet4/static/css/1.3517e91e.chunk.css"
+  },
+  {
+    "revision": "76a0401290bd0fa73966",
+    "url": "/projet4/static/js/1.76a04012.chunk.js"
+  },
+  {
+    "revision": "811af0f3b54fa6435b63",
+    "url": "/projet4/static/js/runtime~main.811af0f3.js"
+  },
+  {
+    "revision": "93b816549063be4d7e158984d7a213b6",
+    "url": "/projet4/static/media/chateau.93b81654.jpg"
+  },
+  {
+    "revision": "b668cc42dd342501607d70569b5e50c3",
+    "url": "/projet4/index.html"
+  }
+];
