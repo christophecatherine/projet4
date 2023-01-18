20acbbca71a86bbc5e12dca6478f9dcb5cc89765
christophecatherine <73233923+christophecatherine@users.noreply.github.com>
2023-01-18 11:37:56 +0100

    first commit

--- src/components/Ui/NavBar/NavBar.js
+++ src/components/Ui/NavBar/NavBar.js
@@ -37,7 +37,7 @@ import LinkContainer from "react-router-bootstrap/lib/LinkContainer";
 const navBar = (props) => (
     <>
         <Navbar bg="dark" variant="dark" expand="lg">
-            <Navbar.Brand href="https://christophecatherine.github.io/">Ariège MGA</Navbar.Brand>
+            <Navbar.Brand href="#home">Ariège MGA</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
