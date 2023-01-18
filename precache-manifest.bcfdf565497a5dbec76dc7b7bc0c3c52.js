12227de950a521859b542d418d7596d0ffd52f98
christophecatherine <73233923+christophecatherine@users.noreply.github.com>
2023-01-18 10:36:09 +0100

    Updates

---
+++ precache-manifest.bcfdf565497a5dbec76dc7b7bc0c3c52.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6b4ae34182fec9dd50f",
+    "url": "christophecatherine.github.io/projet4/static/css/main.33605159.chunk.css"
+  },
+  {
+    "revision": "d6b4ae34182fec9dd50f",
+    "url": "christophecatherine.github.io/projet4/static/js/main.d6b4ae34.chunk.js"
+  },
+  {
+    "revision": "76a0401290bd0fa73966",
+    "url": "christophecatherine.github.io/projet4/static/css/1.3517e91e.chunk.css"
+  },
+  {
+    "revision": "76a0401290bd0fa73966",
+    "url": "christophecatherine.github.io/projet4/static/js/1.76a04012.chunk.js"
+  },
+  {
+    "revision": "fb168022fd1ec7e5eab2",
+    "url": "christophecatherine.github.io/projet4/static/js/runtime~main.fb168022.js"
+  },
+  {
+    "revision": "93b816549063be4d7e158984d7a213b6",
+    "url": "christophecatherine.github.io/projet4/static/media/chateau.93b81654.jpg"
+  },
+  {
+    "revision": "3744198a9d4443f6f0228997d078a69e",
+    "url": "christophecatherine.github.io/projet4/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/projet4/precache-manifest.fed11fe289efcc079484bb48dc7575e7.js"
+  "christophecatherine.github.io/projet4/precache-manifest.bcfdf565497a5dbec76dc7b7bc0c3c52.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/projet4/index.html", {
+workbox.routing.registerNavigationRoute("christophecatherine.github.io/projet4/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
