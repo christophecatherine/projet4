618a859c13a3fc13c10c105b5e187e0cbe81fff1
christophecatherine <73233923+christophecatherine@users.noreply.github.com>
2023-01-18 10:25:00 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+
 import './App.css';
+import React from 'react';
 import Site from "./containers/Site/Site";
 import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
